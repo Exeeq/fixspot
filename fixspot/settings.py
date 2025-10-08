e3cf8bf545c7e98275a0e64e0b38b8e538858206
Exeeq <ex.albornoz@duocuc.cl>
2025-10-07 22:04:00 -0300

    Ajuste en versiones y correcciones

--- fixspot/settings.py
+++ fixspot/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -146,3 +147,9 @@ LOGOUT_REDIRECT_URL = '/'
 # LA CARPETA PARA LAS IMAGENES
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
